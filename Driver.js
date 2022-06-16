c7cf66dd57ade73e700498bfea12712927fae0c4
berzhan.shad <90727526+berzhanshad@users.noreply.github.com>
2022-06-16 06:29:10 -0400

    Add files via upload

--- Driver.js
+++ Driver.js
@@ -1,13 +1,13 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var Account_1 = require("./Account");
 var SavingsAccount_1 = require("./SavingsAccount");
 var LoanAccount_1 = require("./LoanAccount");
 var TravelAccount_1 = require("./TravelAccount");
-var myAccount = new Account_1.Account("Yuki", "Cross", 8221, 5000.00);
-var mySavingsAccount = new SavingsAccount_1.SavingsAccount("Yuki", "Cross", 3255, 4865.00, 1.90);
-var myLoanAccount = new LoanAccount_1.LoanAccount("Yuki", "Cross", 325566, 6300.00, 800.00, 5.88);
-var myTravelAccount = new TravelAccount_1.TravelAccount("Yuki", "Cross", 89568754, 118900.00, "Tokyo", "Air Japan");
+var myAccount = new Account_1.Account("Berzhan", "Shababi Shad", 8221, 5000.00);
+var mySavingsAccount = new SavingsAccount_1.SavingsAccount("Berzhan", "Shababi Shad", 3255, 4865.00, 1.90);
+var myLoanAccount = new LoanAccount_1.LoanAccount("Berzhan", "Shababi Shad", 325566, 6300.00, 800.00, 5.88);
+var myTravelAccount = new TravelAccount_1.TravelAccount("Berzhan", "Shababi Shad", 89568754, 118900.00, "Tokyo", "Air Japan");
 var theAccounts = [];
 theAccounts.push(myAccount);
 theAccounts.push(mySavingsAccount);
--- Driver.ts
+++ Driver.ts
@@ -3,10 +3,10 @@ import {SavingsAccount} from './SavingsAccount';
 import { LoanAccount } from './LoanAccount';
 import { TravelAccount } from './TravelAccount';
 
-let myAccount = new Account("Yuki", "Cross",8221, 5000.00);
-let mySavingsAccount = new SavingsAccount("Yuki", "Cross",3255,4865.00,1.90);
-let myLoanAccount = new LoanAccount("Yuki", "Cross",325566, 6300.00,800.00,5.88);
-let myTravelAccount = new TravelAccount("Yuki", "Cross",89568754,118900.00, "Tokyo","Air Japan");
+let myAccount = new Account("Berzhan", "Shababi Shad",8221, 5000.00);
+let mySavingsAccount = new SavingsAccount("Berzhan", "Shababi Shad",3255,4865.00,1.90);
+let myLoanAccount = new LoanAccount("Berzhan", "Shababi Shad",325566, 6300.00,800.00,5.88);
+let myTravelAccount = new TravelAccount("Berzhan", "Shababi Shad",89568754,118900.00, "Tokyo","Air Japan");
 
 
 let theAccounts: Account[] =[];
